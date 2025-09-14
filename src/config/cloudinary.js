57fab8885d903ca13b54a4cdabfe2b210a9def54
Emediong Bassey Edem <edememediong1@gmail.com>
2025-09-14 20:38:29 +0100

    chore: remove node_modules and .env from repo

--- src/config/cloudinary.js
+++ src/config/cloudinary.js
@@ -1,5 +1,7 @@
-const cloudinary = require('cloudinary').v2;
+const dotenv = require('dotenv')
+dotenv.config()
 
+const cloudinary = require('cloudinary').v2;
 
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
